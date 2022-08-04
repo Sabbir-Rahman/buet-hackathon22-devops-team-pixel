4adef87e2347fd959d281b9dc40064989d7a4715
sakibesrat71 <esratebtida@iut-dhaka.edu>
2022-08-04 21:03:23 +0600

    Testing adding and viewing description

--- backend/admin-server/src/__tests__/admin.test.ts
+++ backend/admin-server/src/__tests__/admin.test.ts
@@ -35,7 +35,8 @@ describe('Auth', () => {
   describe('admin add course route', () => {
     it('should return 200', async () => {
       const response = await request(app).post('/api/v1/admin/course/add')
-      .send({ name: 'test course' })
+      .send({ name: 'test course 4',
+              description: 'jest test description 4'   })
       expect(response.status).toBe(200)
     }
     )
@@ -47,6 +48,7 @@ describe('Auth', () => {
       const response = await request(app).get('/api/v1/admin/course/view')
       expect(response.status).toBe(200)
       expect(response.body.data).toBeInstanceOf(Array)
+      expect(response.body.data[0]).toHaveProperty('description')
       
     }
     )
