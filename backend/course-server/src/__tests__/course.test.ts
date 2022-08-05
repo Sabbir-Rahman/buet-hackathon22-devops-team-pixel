67b46e3555d2577cf244fef132f922f6380a1177
sakibesrat71 <esratebtida@iut-dhaka.edu>
2022-08-05 06:54:59 +0600

    course testing completed

---
+++ backend/course-server/src/__tests__/course.test.ts
@@ -0,0 +1,56 @@
+import { string } from 'zod'
+import request from 'supertest'
+import { MongoMemoryServer } from 'mongodb-memory-server'
+import mongoose from 'mongoose'
+//import jwt from 'jsonwebtoken'
+import createServer from '../api/v1/utils/server'
+
+const app = createServer()
+// testing the auth server
+describe('Auth', () => {
+  beforeAll(async () => {
+    const mongoServer = await MongoMemoryServer.create()
+    await mongoose.connect(mongoServer.getUri())
+  })
+  afterAll(async () => {
+    await mongoose.disconnect()
+    await mongoose.connection.close()
+  })
+  describe('true test', () => {
+    it('should return true', () => {
+      expect(true).toBe(true)
+    }
+    )
+  })
+  // testing enroll course
+    describe('enroll course', () => {
+        it('should return status 200', async () => {
+            const response = await request(app)
+                .post('/api/v1/course/enroll')
+                .send({
+                    studentId: '5e9f8f8f8f8f8f8f8f8f8f8',
+                    courseId: '5e9f8f8f8f8f8f8f8f8f8f8'
+                })
+                .set('Accept', 'application/json')
+                .expect(200)
+            expect(response.body.isSuccess).toBe(true)
+            expect(response.body.statusCode).toBe(200)
+        }
+        )
+    }
+    )
+    // testing view enrolled course
+    describe('view enrolled course', () => {
+        it('should return status 200', async () => {
+            const response = await request(app)
+                .get('/api/v1/course/view')
+                .set('Accept', 'application/json')
+                .expect(200)
+            expect(response.body.isSuccess).toBe(true)
+            expect(response.body.statusCode).toBe(200)
+            expect(response.body.data).toBeInstanceOf(Array)
+            expect(response.body.data[0]).toHaveProperty('courseId')
+        }
+        )
+    })
+})
---
+++ backend/course-server/src/api/v1/utils/server.ts
@@ -0,0 +1,18 @@
+import express, { Response } from 'express'
+import cors from 'cors'
+
+import { v1Router } from '..'
+
+function createServer() {
+  const app = express()
+  app.use(cors())
+  app.use(express.json())
+  app.get('/', (req, res) => {
+    res.send('Welcome from auth-server ci-cd pipeline')
+  })
+  app.use('/api/v1/course', v1Router)
+
+  return app
+}
+
+export default createServer
